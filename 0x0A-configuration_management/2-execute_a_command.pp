ea256c00795b8b82abece0abd29266506721e9e5
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-21 22:56:31 -0700

    tasks 0:2

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+#create a manifest that kills a process named killmenow
+
+
+
+exec {'pkill':
+command => 'pkill -f killmenow',
+path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,2 @@
+We learn configuration_management using Puppet by solving 3 tasks
+ 
