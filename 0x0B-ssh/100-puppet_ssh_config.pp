8ca750692fea309f7b00f6a45ba2f64dd2233ce2
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-23 18:42:56 +0300

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # set up your client SSH configuration file
 
-file {'~/.ssh/config':
-ensure => "present"
-path => "~/etc/ssh/ssh_config",
-content => "
+file {'~/etc/ssh/ssh_config':
+ensure => 'present'
+path => '~/etc/ssh/ssh_config',
+content => '
 Host *
         IdentityFile ~/.ssh/school
         PasswordAuthentication no
-        ",
+        ',
 }
