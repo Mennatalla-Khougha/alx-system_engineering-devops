f1e635f7fe03136be01150c29ed0ad28d807bfa9
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-23 18:40:15 +0300

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,8 @@
 # set up your client SSH configuration file
 
 file_line {'sudo_rule':
-path => '~/.ssh/config',
+path => '/etc/ssh/ssh_config',
 line => '
-        Host 100.25.111.125
-        HostName 100.25.111.125
-        User ubuntu
         IdentityFile ~/.ssh/school
         PasswordAuthentication no
         ',
