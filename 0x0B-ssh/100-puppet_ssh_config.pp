da3e71a7131392fc4d7dce3e17380ab117d2b775
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-23 18:38:49 +0300

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,6 +4,7 @@ file_line {'sudo_rule':
 path => '~/.ssh/config',
 line => '
         Host 100.25.111.125
+        HostName 100.25.111.125
         User ubuntu
         IdentityFile ~/.ssh/school
         PasswordAuthentication no
