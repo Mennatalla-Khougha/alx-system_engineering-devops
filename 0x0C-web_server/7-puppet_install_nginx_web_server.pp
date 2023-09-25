878adbfbc259483c9931db8e476f011a055d742e
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-25 23:13:57 +0300

    task 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,33 @@
+#  configuring your server with Puppet
+
+package {'nginx'
+ensure => 'installed',
+name => 'nginx',
+}
+
+file {'/var/www/html/index.html'
+path => '/var/www/html/index.html',
+ensure => 'present',
+content => '<h1>Hello, world!</h1>', 
+}
+
+file {'/var/www/html/404.html'
+path => '/var/www/html/404.html',
+ensure => 'present',
+content => 'Ceci n'est pas une page',
+}
+
+exec {'sed'
+command => sed -i '/server_name _;/a \
+\
+    location /redirect_me {\
+    return 301 https://github.com/Mennatalla-Khougha; \
+    } \
+\
+    error_page 404 /404.html; \
+    location =/404.html { \
+    root /var/www/html; \
+    internal; \
+    }',
+path => '/etc/nginx/sites-available/default'
+}
