835876ae6c2dded454b567ec349dbc17f80035d3
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-02 13:23:22 +0300

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,26 @@
-# Install and config the nginx
+#  configuring your server with Puppet
 
-
-package { 'nginx':
-  ensure  => installed,
+exec { 'update':
+  command  => 'sudo apt-get update',
 }
 
-file_line { 'headercustom':
-  ensure  => present,
-  path    => '/etc/nginx/sites-available/default',
-  after   => ':80 default_server;',
-  line    => "add_header X-Served-By ${hostname};",
-  require => Package['nginx'],
+package {'nginx':
+ensure => 'installed',
+}
+
+file {'/etc/nginx/sites-available/default':
+ensure => 'present',
+path   => '/etc/nginx/sites-available/default',
+}
+
+file_line {'header':
+    ensure => 'present',
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'listen [::]:80 default_server;',
+    line   => 'add_header X-Served-By ${hostname};',
 }
 
 service { 'nginx':
   ensure  => running,
-  require => File_line['headercustom'],
+  require => File_line['header'],
 }
