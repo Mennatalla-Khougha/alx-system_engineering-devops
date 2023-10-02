6a753d117e7140e86a3db241641bc4b27c75a7e7
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-02 13:34:15 +0300

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -15,10 +15,11 @@ path   => '/etc/nginx/sites-available/default',
 }
 
 file_line {'header':
-    ensure => 'present',
-    path   => '/etc/nginx/sites-available/default',
-    after  => 'listen [::]:80 default_server;',
-    line   => 'add_header X-Served-By ${hostname};',
+    ensure  => 'present',
+    path    => '/etc/nginx/sites-available/default',
+    after   => 'listen [::]:80 default_server;',
+    line    => 'add_header X-Served-By ${hostname};',
+    require => Package['nginx'],
 }
 
 exec {'restart':
