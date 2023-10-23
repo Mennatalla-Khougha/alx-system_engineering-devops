ab41c064a73f10e9948bd6f115b31353a752051f
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-23 13:18:33 +0300

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""This script retrieves and displays a user's TODO list from a REST API."""
+import csv
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    employee_ID = argv[1]
+    api = f"https://jsonplaceholder.typicode.com/"
+    user_info = f"users/{employee_ID}"
+    to_do_list = f"todos?userId={employee_ID}"
+
+    response = get(api + user_info)
+    employee_info = response.json()
+    response = get(api + to_do_list)
+    tasks = response.json()
+
+    employee_name = f"Employee {employee_info['name']}"
+    completed_task = [task for task in tasks if task['completed']]
+    no_comp_task = len(completed_task)
+    no_tasks = len(tasks)
+
+    file_name = f"{employee_ID}.csv"
+    with open(file_name, 'w', newline="") as file:
+        file = csv.writer(file, quoting=csv.QUOTE_ALL)
+        file.writerow(["USER_ID",
+                       "USERNAME",
+                       "TASK_COMPLETED_STATUS",
+                       "TASK_TITLE"])
+        for task in tasks:
+            file.writerow([str(employee_ID),
+                           str(employee_info['username']),
+                           str(task['completed']),
+                           str(task['title'])])
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -3,3 +3,7 @@
 ## Task 1
 
 Python script that retrieves and displays a user's TODO list progress from a REST API.
+
+## Task 2
+
+Python script that retrieves and displays a user's TODO list progress from a REST API and export data in the CSV format.
