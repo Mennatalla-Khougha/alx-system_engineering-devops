7af0ffd9d2f0b5e1a9574c6e48b7069f6a081c55
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-23 14:50:34 +0300

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -20,12 +20,8 @@ if __name__ == "__main__":
         tasks = response.json()
 
         employee_data = [{"username": employee_info['username'],
-                         "task": task['title'],
-                         "completed": task['completed']} for task in tasks]
-
-        # for task in tasks:
-        #     employee_data["tasks"].append({"task": task['title'],
-        #                                    "completed": task['completed']})
+                         "task": task['title'], "completed": task['completed']}
+                         for task in tasks]
 
         data[user] = employee_data
     with open("todo_all_employees.json", 'w', ) as file:
