e9d0e0357c43102b0c2b34a6633def6139e9e381
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-11-07 14:39:50 +0200

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Script that queries the Reddit API and returns a list of all hot titles"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """queries the Reddit API and returns a list of all hot titles"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
+    header = {'User-agent': 'ALX project'}
+    if after:
+        url += f"&after={after}"
+    response = requests.get(url, headers=header, allow_redirects=False)
+    print(response.status_code)
+    if response.status_code != 200:
+        return None
+    data = response.json()
+    posts = data['data']['children']
+    if not posts:
+        return hot_list
+    for post in posts:
+        hot_list.append(post['data']['title'])
+    after = data['data']['after']
+    return recurse(subreddit, hot_list, after)
