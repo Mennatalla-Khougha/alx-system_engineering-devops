20d1d0858ef02e002ca99d5f6af9c92e26e8ab89
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-11-07 13:16:35 +0200

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False)
-    print(response.status_code)
-    if response.status_code != 200 :
+    if response.status_code != 200:
         return 0
-    
+
     data = response.json()
     print(data['data']["subscribers"])
     return data['data']["subscribers"]
