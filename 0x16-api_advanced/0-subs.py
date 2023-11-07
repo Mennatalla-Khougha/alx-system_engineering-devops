04966949921e216287ef2ac43280a01142fc5c55
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-11-07 13:28:57 +0200

    adding header to the request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,10 @@ import requests
 def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False)
+    header = {'User-agent': 'ALX project'}
+    response = requests.get(url, headers=header, allow_redirects=False)
     if response.status_code != 200:
         return 0
 
     data = response.json()
-    print(data['data']["subscribers"])
     return data['data']["subscribers"]
