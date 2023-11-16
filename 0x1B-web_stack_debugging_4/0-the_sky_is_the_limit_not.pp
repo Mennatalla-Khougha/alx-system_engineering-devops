afeecb1f93c8b569fe38a00fbd5165d7e96bd2c5
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-11-16 12:10:34 +0200

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
-# fixing the ulimit 
+# fixing the limit of traffic 
 
+# Increases the ulimit
 exec { 'fix-ulimit':
     command => 'sed -i "s/15/1024/" /etc/default/nginx',
     path => '/bin/:/usr/local/bin',
