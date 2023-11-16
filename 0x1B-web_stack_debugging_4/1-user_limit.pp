b02f554ac115c85058a3635fdf1e67a68fee77ca
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-11-16 12:43:57 +0200

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,11 +3,11 @@
 # increase the soft limit
 exec {'soft-limit':
     command => "sed -i "holberton soft nofile s/4/4096" /etc/security/limits.conf'
-    path   => '/bin:/usr/local/bin'
+    path   => '/bin/:/usr/local/bin/'
 }
 
 # increase the hard limit
 exec {'soft-limit':
     command => "sed -i "holberton hard nofile s/5/4096" /etc/security/limits.conf'
-    path   => '/bin:/usr/local/bin'
+    path   => '/bin/:/usr/local/bin/'
 }
