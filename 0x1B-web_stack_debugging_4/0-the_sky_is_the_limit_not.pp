9675c5d9ec44ad7b1c68d235b9b32cb10326cca6
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-11-16 12:08:39 +0200

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# fixing the ulimit 
+
+exec { 'fix-ulimit':
+    command => 'sed -i "s/15/1024/" /etc/default/nginx',
+    path => '/bin/:/usr/local/bin',
+}
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => '/usr/sbin/service nginx restart',
+  require => Exec['fix-ulimit'],
+}
